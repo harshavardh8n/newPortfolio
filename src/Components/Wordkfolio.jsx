7117ebf84056182a61f66a29b08dc95781b9f52e
harshavardh8n <harshavardhangaikwad39@gmail.com>
2024-09-06 20:55:24 +0530

    errors

--- src/Components/Wordkfolio.jsx
+++ src/Components/Wordkfolio.jsx
@@ -11,7 +11,7 @@ const Wordkfolio = ({lightmode}) => {
     <div>
 
         <Element name="workfolio">
-        <Box bgColor={"#F5F5F5"} padding={"5rem"} minHeight={"100vh"}>
+        <Box bgColor={"#F5F5F5"} padding={"5rem"} minHeight={"100vh"} maxWidth={"100vw"}>
         <Text fontSize="4xl" as="b" margin={"2rem"}>Workfolio</Text>
         <TabsComponent lightMode={"true"}/>
         {/* <Card tag="web dev" title={"Resumine"} description={"Analyze, Compare or visualise your resume"} image={"https://scontent.fpnq13-1.fna.fbcdn.net/v/t39.30808-6/230518893_131833929101053_5303653532477360408_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=5NkgP9oMkFcQ7kNvgFW0Rcj&_nc_ht=scontent.fpnq13-1.fna&oh=00_AYAIw64cvjbePWPCYSIOHPMTvazt4g4qQJsxTs3V_XXh2w&oe=66E055F3" } link="https://resumine-frontend.vercel.app/" lightMode={"true"}/> */}
